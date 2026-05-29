811b78c143730d7617aad410f8be70a45b5b3190
Anatoliy Shekera <tolikshekera@gmail.com>
2026-05-29 23:53:15 +0300

    scheduler

--- scheduler.py
+++ scheduler.py
@@ -9,7 +9,7 @@ from datetime import datetime
 
 
 bot = Bot(token='8765817228:AAHdW5T3kIozIK4ZuMVRZL-lb2DB0vqrmsc')
-CHAT_ID = 304642547  # your chat or channel ID
+CHAT_ID = 304642547 
 
 async def send_hourly_report():
     
@@ -19,7 +19,7 @@ async def main():
     scheduler = AsyncIOScheduler()
     scheduler.add_job(send_hourly_report, "interval", hours=1, next_run_time=datetime.now())
     scheduler.start()
-    await asyncio.Event().wait()  # run forever
+    await asyncio.Event().wait()
 
 if __name__ == "__main__":
     asyncio.run(main())
